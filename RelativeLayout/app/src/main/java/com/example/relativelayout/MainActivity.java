97da3a78fefb14685f820af6d915e9e7e73a2832
pttnguyet73 <pnguyet1992@gmail.com>
2024-09-12 14:15:22 +0700

    bt2

---
+++ RelativeLayout/app/src/main/java/com/example/relativelayout/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.relativelayout;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                String ten = username.getText().toString();
+                String pass = password.getText().toString();
+                Log.i("Test credentials", "Username: " + username + " and Password: " +password);
+
+            }
+        });
+    }
+
+}
